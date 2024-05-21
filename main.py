0283f7cf8a5759e5a9862be46c680e63e2ad471a
Prathyarthi <prathyarti@gmail.com>
2024-05-21 22:28:48 +0530

    Call the functions, data scraping works and render it onto the frontend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, extract_body_content,clean_body_content,split_dom_content
 
 st.title("AI WEB SCRAPER")
 url = st.text_input("Enter a website URL: ")
@@ -7,5 +7,12 @@ url = st.text_input("Enter a website URL: ")
 if st.button("Scrape Site"):
     st.write("Scraping site...")
     result = scrape_website(url)
-    print(result)
+
+    body_content = extract_body_content(result)
+    cleaned_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = cleaned_content
+
+    with st.expander("Show DOM Content"):
+        st.text_area("DOM content",cleaned_content,height=300)
 
