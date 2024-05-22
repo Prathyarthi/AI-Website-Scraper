3ce26bd5420bd0ca1c56a569b9bd7f8c16213f23
Prathyarthi <prathyarti@gmail.com>
2024-05-22 19:23:36 +0530

    Called the parse function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrape_website, extract_body_content,clean_body_content,split_dom_content
+from parse import parse_with_ollama
 
 st.title("AI WEB SCRAPER")
 url = st.text_input("Enter a website URL: ")
@@ -15,4 +16,15 @@ if st.button("Scrape Site"):
 
     with st.expander("Show DOM Content"):
         st.text_area("DOM content",cleaned_content,height=300)
+
+if "dom_content" in st.session_state:
+    parse_descripton = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_descripton:
+            st.write("Parsing the content...")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks,parse_descripton)
+            st.write(result)
 
